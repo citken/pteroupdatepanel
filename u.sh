4a565ff9e25cdf6ef4977ff684bb050018df5a01
citken <86873662+citken@users.noreply.github.com>
2022-10-07 16:56:16 +0700

    Add files via upload

---
+++ u.sh
@@ -0,0 +1,16 @@
+echo "Pterodactyl Panel Updater BY KEYZALþÿ
+echo "Config Starting"
+rm /root
+apt install speedtest-cli -y
+speedtest
+cd /var/www/pterodactyl
+php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan queue:restart
+php artisan up
